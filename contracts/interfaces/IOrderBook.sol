9d3456bc6a37d83737925c727ab3f53dab3f0305
cherideal <101du@163.com>
2021-12-06 17:08:19 +0800

    draft 202111301116

--- contracts/interfaces/IOrderBook.sol
+++ contracts/interfaces/IOrderBook.sol
@@ -72,12 +72,12 @@ interface IOrderBook {
     //更新最小数量
     function minAmountUpdate(uint newMinAmount) external;
 
-    function getAmountOutForMovePrice(uint amountInOffer, uint reserveIn, uint reserveOut)
+    function getAmountOutForMovePrice(address tokenIn, uint amountInOffer, uint reserveIn, uint reserveOut)
     external
     view
     returns (uint amountOutGet, uint amountInLeft, uint reserveInRet, uint reserveOutRet);
 
-    function getAmountInForMovePrice(uint amountOutOffer, uint reserveIn, uint reserveOut)
+    function getAmountInForMovePrice(address tokenOut, uint amountOutOffer, uint reserveIn, uint reserveOut)
     external
     view
     returns (uint amountInGet, uint amountOutLeft, uint reserveInRet, uint reserveOutRet);
