5a4a06b36177de6db41c2f044f0379336ff41ae2
cherideal <101du@163.com>
2021-12-22 16:01:02 +0800

    test case for order book

--- contracts/OrderBook.sol
+++ contracts/OrderBook.sol
@@ -146,42 +146,6 @@ contract OrderBook is OrderBookBase {
         IUniswapV2Pair(pair).sync();
     }
 
-    function _getFixAmountForMovePriceUp(uint _amountLeft, uint _amountAmmQuote,
-        uint reserveBase, uint reserveQuote, uint targetPrice)
-    internal view returns (uint amountLeft, uint amountAmmQuote) {
-        uint curPrice = OrderBookLibrary.getPrice(reserveBase, reserveQuote, priceDecimal);
-        //弥补精度损失造成的LP价格误差，将LP的价格提高一点，保证买单价格小于或等于LP价格
-        //y' = x.p2 - x.p1, x不变，增加y, 使用价格变大
-        if (curPrice < targetPrice) {
-            uint amountQuoteFix = (reserveBase.mul(targetPrice).div(10 ** priceDecimal)
-                .sub(reserveBase.mul(curPrice).div(10 ** priceDecimal)));
-            amountQuoteFix = amountQuoteFix > 0 ? amountQuoteFix : 1;
-            require(_amountLeft >= amountQuoteFix, "Hybridx OrderBook: Not Enough Output Amount");
-            (amountLeft, amountAmmQuote) = (_amountLeft.sub(amountQuoteFix), _amountAmmQuote + amountQuoteFix);
-        }
-        else {
-            (amountLeft, amountAmmQuote) = (_amountLeft, _amountAmmQuote);
-        }
-    }
-
-    function _getFixAmountForMovePriceDown(uint _amountLeft, uint _amountAmmBase,
-        uint reserveBase, uint reserveQuote, uint targetPrice)
-    public view returns (uint amountLeft, uint amountAmmBase) {
-        uint curPrice = OrderBookLibrary.getPrice(reserveBase, reserveQuote, priceDecimal);
-        //弥补精度损失造成的LP价格误差，将LP的价格降低一点，保证订单价格大于或等于LP价格
-        //x' = y/p1 - y/p2, y不变，增加x，使价格变小
-        if (curPrice > targetPrice) {
-            uint amountBaseFix = (reserveQuote.mul(10 ** priceDecimal).div(targetPrice)
-            .sub(reserveQuote.mul(10 ** priceDecimal).div(curPrice)));
-            amountBaseFix = amountBaseFix > 0 ? amountBaseFix : 1;
-            require(_amountLeft >= amountBaseFix, "Hybridx OrderBook: Not Enough Input Amount");
-            (amountLeft, amountAmmBase) = (_amountLeft.sub(amountBaseFix), _amountAmmBase + amountBaseFix);
-        }
-        else {
-            (amountLeft, amountAmmBase) = (_amountLeft, _amountAmmBase);
-        }
-    }
-
     /*
         swap to price1 and take the order with price of price1 and
         swap to price2 and take the order with price of price2
@@ -246,7 +210,8 @@ contract OrderBook is OrderBookBase {
         if (amountAmmQuote > 0) {
             if (amountLeft > 0) {
                 (amountLeft, amountAmmQuote) =
-                    _getFixAmountForMovePriceUp(amountLeft, amountAmmQuote, reserveBase, reserveQuote, targetPrice);
+                    OrderBookLibrary.getFixAmountForMovePriceUp(amountLeft, amountAmmQuote, reserveBase, reserveQuote,
+                        targetPrice, priceDecimal);
             }
 
             _ammSwapPrice(to, quoteToken, baseToken, amountAmmQuote, amountAmmBase);
@@ -319,7 +284,8 @@ contract OrderBook is OrderBookBase {
         if (amountAmmBase > 0) {
             if (amountLeft > 0) {
                 (amountLeft, amountAmmBase) =
-                _getFixAmountForMovePriceDown(amountLeft, amountAmmBase, reserveBase, reserveQuote, targetPrice);
+                    OrderBookLibrary.getFixAmountForMovePriceDown(amountLeft, amountAmmBase, reserveBase, reserveQuote,
+                        targetPrice, priceDecimal);
             }
 
             _ammSwapPrice(to, baseToken, quoteToken, amountAmmBase, amountAmmQuote);
